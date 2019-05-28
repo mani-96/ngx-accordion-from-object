0dc4ed915c7e2330ce3a4aa1c9dfe13327c8f603
Manish Sharma <msmanishsharma52@gmail.com>
2019-05-28 11:37:35 +0530

    Removed Browser module from imports

--- projects/ngx-accordion-from-object/src/lib/ngx-accordion-from-object.module.ts
+++ projects/ngx-accordion-from-object/src/lib/ngx-accordion-from-object.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common'; 
 import { NgxAccordionFromObjectComponent, AccordionTab } from './ngx-accordion-from-object.component';
 
 @NgModule({
   declarations: [NgxAccordionFromObjectComponent, AccordionTab],
-  imports: [
-    CommonModule,
-    BrowserModule
-  ],
+  imports: [ CommonModule ],
   exports: [NgxAccordionFromObjectComponent, AccordionTab]
 })
 export class NgxAccordionFromObjectModule { }
